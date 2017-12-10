3a19931e37cd146f7eec3185ae2ca5dbc403c6e6
Angelica Valenta <angelicavalenta@gmail.com>
2017-12-10 12:30:51 -0500

    feat: mail

---
+++ contact.php
@@ -0,0 +1,20 @@
+
+<?php
+if($_POST["message"]) {
+    mail("angelica@seaworthy.tech", "Form to email message", $_POST["message"], "From: an@email.address");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form method="post" action="contact.php">
+        <textarea name="message"></textarea>
+        <input type="submit">
+    </form>
+  </body>
+</html>
